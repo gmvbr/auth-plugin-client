5971998cccb643ddf727d52b45021368e164aca3
gmvbr <72897639+gmvbr@users.noreply.github.com>
2020-10-16 06:55:11 -0400

    remove 'eslint-disable node/no-unpublished-import'

--- src/defaults/auth-error.ts
+++ src/defaults/auth-error.ts
@@ -1,4 +1,3 @@
-/* eslint-disable node/no-unpublished-import */
 import {FastifyReply, FastifyRequest} from 'fastify';
 import {AuthError} from '../models/auth-error';
 
--- src/models/auth-model.ts
+++ src/models/auth-model.ts
@@ -1,4 +1,3 @@
-/* eslint-disable node/no-unpublished-import */
 import {FastifyReply, FastifyRequest} from 'fastify';
 
 abstract class AuthModel {
